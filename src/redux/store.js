d04bce9a8eb33a35291b2da6a53cbb79189a3b94
Serhiy Malysh <166062683+finconsultsm@users.noreply.github.com>
2025-01-30 08:07:54 +0100

    1

---
+++ src/redux/store.js
@@ -0,0 +1,26 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import contactsSlice from "./contactsSlice";
+import filtersSlice from "./filtersSlice";
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice,
+    filter: filtersSlice,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
